9877215b7ed98564dca7d69c3bd86268671ac3d9
katherine011 <guluaq09@gmail.com>
2025-04-10 19:10:28 +0400

    done

--- src/app/(pages)/userprofile/page.tsx
+++ src/app/(pages)/userprofile/page.tsx
@@ -1,15 +1,15 @@
-import Border2 from "@/app/components/__atoms/Borders/Border2";
-import UserProfile from "@/app/components/__molecules/UserProfile";
 import React from "react";
+import dynamic from "next/dynamic";
+
+const UserProfile = dynamic(
+  () => import("../../components/__molecules/UserProfile"),
+  { ssr: false }
+);
 
 const Page = () => {
   return (
     <div className="bg-[#F2F3F5] w-[100%] h-[100vh] flex flex-row items-center justify-center ">
-      <Border2 show pagename="Profile">
-        <>
-          <UserProfile />
-        </>
-      </Border2>
+      <UserProfile />
     </div>
   );
 };
